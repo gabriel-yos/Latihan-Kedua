752799d9c3a5163791d276b116409909b280cf5e
Gabriel Alexander <gabriel.alexander@sma-yosuka.sch.id>
2024-07-24 11:27:53 +0700

    Update Perhitungan Array Multidimensi.cpp

--- Perhitungan Array Multidimensi.cpp
+++ Perhitungan Array Multidimensi.cpp
@@ -6,22 +6,22 @@ int main()
 {
 	//mendeklarasikan array dalam variable
 	int A[2][2], B[2][2], C[2][2];
-	cout << "Masukkan Nikal A11 : ";
+	cout << "Masukkan Nilai A11 : ";
 	cin >> A[0][0];
-	cout << "Masukkan Nikal A12 : ";
+	cout << "Masukkan Nilai A12 : ";
 	cin >> A[0][1];
-	cout << "Masukkan Nikal A21 : ";
+	cout << "Masukkan Nilai A21 : ";
 	cin >> A[1][0];
-	cout << "Masukkan Nikal A22 : ";
+	cout << "Masukkan Nilai A22 : ";
 	cin >> A[1][1];
 
-	cout << "Masukkan Nikal B11 : ";
+	cout << "Masukkan Nilai B11 : ";
 	cin >> B[0][0];
-	cout << "Masukkan Nikal B12 : ";
+	cout << "Masukkan Nilai B12 : ";
 	cin >> B[0][1];
-	cout << "Masukkan Nikal B21 : ";
+	cout << "Masukkan Nilai B21 : ";
 	cin >> B[1][0];
-	cout << "Masukkan Nikal B22 : ";
+	cout << "Masukkan Nilai B22 : ";
 	cin >> B[1][1];
 
 	//menghitung nilai array
